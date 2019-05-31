27a7eabceb989dfe052a4a9e11a777eb45933d9e
sdaemen <sandor.daemen@gmail.com>
2019-05-31 14:46:23 +0200

    Initial push

---
+++ Assets/Editor/Scripts/TutorialShortcutCustomEditor.cs
@@ -0,0 +1,107 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+//[CustomEditor(typeof(TutorialShortcutData))]
+public class TutorialShortcutCustomEditor : Editor
+{
+    private SerializedProperty m_shortcutData;
+    private ReorderableList m_ReorderableList;
+    
+    private void OnEnable()
+    {
+        //Find the list in our ScriptableObject script.
+        m_shortcutData = serializedObject.FindProperty("shortcuts");
+        
+        //Create an instance of our reorderable list.
+        m_ReorderableList = new ReorderableList(serializedObject: serializedObject, elements: m_shortcutData, draggable: true, displayHeader: true,
+            displayAddButton: true, displayRemoveButton: true);
+
+        //Set up the method callback to draw our list header
+        m_ReorderableList.drawHeaderCallback = DrawHeaderCallback;
+
+        //Set up the method callback to draw each element in our reorderable list
+        m_ReorderableList.drawElementCallback = DrawElementCallback;
+
+        //Set the height of each element.
+        m_ReorderableList.elementHeightCallback += ElementHeightCallback;
+
+        //Set up the method callback to define what should happen when we add a new object to our list.
+        m_ReorderableList.onAddCallback += OnAddCallback;
+    }
+    
+    /// <summary>
+    /// Draws the header for the reorderable list
+    /// </summary>
+    /// <param name="rect"></param>
+    private void DrawHeaderCallback(Rect rect)
+    {
+        EditorGUI.LabelField(rect, "Shortcuts");
+    }
+    
+    /// <summary>
+    /// This methods decides how to draw each element in the list
+    /// </summary>
+    /// <param name="rect"></param>
+    /// <param name="index"></param>
+    /// <param name="isactive"></param>
+    /// <param name="isfocused"></param>
+    private void DrawElementCallback(Rect rect, int index, bool isactive, bool isfocused)
+    {
+        //Get the element we want to draw from the list.
+        SerializedProperty element = m_ReorderableList.serializedProperty.GetArrayElementAtIndex(index);
+        rect.y += 2;
+    
+        //We get the name property of our element so we can display this in our list.
+        SerializedProperty elementName = element.FindPropertyRelative("m_Name");
+        string elementTitle = string.IsNullOrEmpty(elementName.stringValue)
+            ? "New Shortcut"
+            : $"Shortcut: {elementName.stringValue}";
+
+        //Draw the list item as a property field, just like Unity does internally.
+        EditorGUI.PropertyField(position:
+            new Rect(rect.x += 10, rect.y, Screen.width * .8f, height: EditorGUIUtility.singleLineHeight), property:
+            element, label: new GUIContent(elementTitle), includeChildren: true);
+    }
+    
+    /// <summary>
+    /// Calculates the height of a single element in the list.
+    /// This is extremely useful when displaying list-items with nested data. 
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    private float ElementHeightCallback(int index)
+    {
+        //Gets the height of the element. This also accounts for properties that can be expanded, like structs.
+        float propertyHeight =
+            EditorGUI.GetPropertyHeight(m_ReorderableList.serializedProperty.GetArrayElementAtIndex(index), true);
+        
+        float spacing = EditorGUIUtility.singleLineHeight / 2;
+        
+        return propertyHeight + spacing;
+    }
+    
+    /// <summary>
+    /// Defines how a new list element should be created and added to our list.
+    /// </summary>
+    /// <param name="list"></param>
+    private void OnAddCallback(ReorderableList list)
+    {
+        var index = list.serializedProperty.arraySize;
+        list.serializedProperty.arraySize++;
+        list.index = index;
+        var element = list.serializedProperty.GetArrayElementAtIndex(index);
+    }
+
+    /// <summary>
+    /// Draw the Inspector Window
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        m_ReorderableList.DoLayoutList();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/TutorialShortcutData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(order = 0, fileName = "TutorialShortcutData", menuName = "Shortcut Data")]
+public class TutorialShortcutData : ScriptableObject
+{
+    public List<ShortcutDataRecord> shortcuts;
+
+    [System.Serializable]
+    public class ShortcutDataRecord
+    {
+        [SerializeField]
+        private string m_Name;
+
+        [FormerlySerializedAs("m_KeyboardRecord")]
+        [SerializeField]
+        private Binding m_Binding;
+
+        public string name => m_Name;
+
+        public Binding binding => m_Binding;
+    }
+
+    [System.Serializable]
+    public struct Binding
+    {
+        [SerializeField]
+        private KeyCode m_KeyCode;
+
+        [SerializeField]
+        private EventModifiers m_Modifiers;
+
+
+        public Binding(KeyCode keyCode, EventModifiers modifiers) : this()
+        {
+            this.m_KeyCode = keyCode;
+            this.m_Modifiers = modifiers;
+        }
+
+        public KeyCode keyCode => m_KeyCode;
+
+        public EventModifiers modifiers => m_Modifiers;
+
+        public static Binding none => new Binding();
+    }
+}
